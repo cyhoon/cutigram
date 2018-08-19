308c3b7c6cea3bd47d1771f8b7feb0ab319475da
JeffChoi <ihello0720@gmail.com>
2018-08-19 19:55:17 +0900

    [Backend][Update] User Model

--- backend/src/main/java/jeff/cutigram/database/model/User.java
+++ backend/src/main/java/jeff/cutigram/database/model/User.java
@@ -1,20 +1,25 @@
 package jeff.cutigram.database.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
-    @Column(name = "id", length = 50)
+    @Column(name = "id", length = 50, nullable = false)
     private String id;
 
     @Column(nullable = false, length = 200)
@@ -34,6 +39,43 @@ public class User {
 
     @Column(name = "photo_src", length = 300)
     private String photoSrc;
+
+    @Builder
+    public User(String id, String password, String displayName) {
+        this.id = id;
+        this.password = password;
+        this.displayName = displayName;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return displayName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 //    @OneToMany(mappedBy = "board")
 //    private Board board;
