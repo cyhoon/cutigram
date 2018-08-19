4524e5a56f317be505413dd1a3b5f895f386ee45
JeffChoi <ihello0720@gmail.com>
2018-08-19 19:53:48 +0900

    [Backend][Add] AuthController

--- backend/src/main/java/jeff/cutigram/controller/AuthController.java
+++ backend/src/main/java/jeff/cutigram/controller/AuthController.java
@@ -1,29 +1,82 @@
 package jeff.cutigram.controller;
 
-import jeff.cutigram.dto.auth.RegisterDto;
+import jeff.cutigram.database.model.User;
+import jeff.cutigram.exception.AuthenticationException;
+import jeff.cutigram.payload.request.LoginRequest;
+import jeff.cutigram.payload.request.RegisterRequest;
+import jeff.cutigram.payload.response.TokenResponse;
+import jeff.cutigram.security.JwtTokenProvider;
+import jeff.cutigram.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Objects;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    @GetMapping("/")
-    public String test() {
-        return "test";
-    }
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/login")
-    public String login() {
-        return "login";
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUserId(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        final String token = jwtTokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new TokenResponse(token));
     }
 
     @PostMapping("/register")
-    public String register(@Valid @RequestBody RegisterDto dto) {
-        final String password = dto.getPassword();
-        System.out.println("여기 안옴?");
-        return "register";
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        final String planTextPassword = registerRequest.getPassword();
+
+        User user = userService.createUser(registerRequest);
+        Authentication authentication = authenticate(user.getId(), planTextPassword);
+
+        final String token = jwtTokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new TokenResponse(token));
+    }
+
+    private Authentication authenticate(String userId, String password) throws UsernameNotFoundException {
+        Objects.requireNonNull(userId);
+        Objects.requireNonNull(password);
+
+        Authentication authentication;
+
+        try {
+            return authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userId, password));
+        } catch (DisabledException e) {
+            throw new AuthenticationException("User is disabled!", e);
+        } catch (BadCredentialsException e) {
+            throw new AuthenticationException("Bad credentials!", e);
+        }
     }
 }
