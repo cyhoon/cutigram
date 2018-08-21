e02b0e10071bd44c3789fdec0ec57a6dc772e585
JeffChoi <ihello0720@gmail.com>
2018-08-21 20:58:49 +0900

    [Backend][Modify] BoardLike

--- backend/src/main/java/jeff/cutigram/database/model/BoardLike.java
+++ backend/src/main/java/jeff/cutigram/database/model/BoardLike.java
@@ -1,16 +1,26 @@
 package jeff.cutigram.database.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class BoardLike {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int idx;
 
     @Column(name = "push_date")
-    private LocalDateTime pushDate = LocalDateTime.now();
+    @CreationTimestamp
+    private LocalDateTime pushDate;
 
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false, updatable = false)
@@ -19,4 +29,10 @@ public class BoardLike {
     @ManyToOne
     @JoinColumn(name = "board_idx", referencedColumnName = "idx", nullable = false, updatable = false)
     private Board board;
+
+    @Builder
+    public BoardLike(Board board, User user) {
+        this.board = board;
+        this.user = user;
+    }
 }
